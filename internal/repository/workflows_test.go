2bc67f73e20a25b76e98d9cffe36d095da60f37c
wkj2333666 <2695071759@qq.com>
2026-07-31 22:45:34 +0800

    test: verify complete dependency license

--- internal/repository/workflows_test.go
+++ internal/repository/workflows_test.go
@@ -64,15 +64,24 @@ func TestThirdPartyNoticesContainsCoderWebSocketLicense(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	for _, required := range []string{
-		"github.com/coder/websocket v1.8.15",
-		"Copyright (c) 2025 Coder",
-		"Permission to use, copy, modify, and distribute this software",
-		`THE SOFTWARE IS PROVIDED "AS IS"`,
-	} {
-		if !strings.Contains(string(content), required) {
-			t.Errorf("THIRD_PARTY_NOTICES missing %q", required)
-		}
+	if !strings.Contains(string(content), "github.com/coder/websocket v1.8.15") {
+		t.Error("THIRD_PARTY_NOTICES missing pinned coder/websocket version")
+	}
+	const license = `Copyright (c) 2025 Coder
+
+Permission to use, copy, modify, and distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.`
+	if !strings.Contains(string(content), license) {
+		t.Error("THIRD_PARTY_NOTICES does not contain the complete coder/websocket ISC license")
 	}
 }
 
